a9abf11ddd3bca00992bb9469c496e4173a1b1b7
yugykim <82011274+yugykim@users.noreply.github.com>
2022-01-04 23:48:05 -0700

    created app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+//Select DOM
+const todoInput = document.querySelector(".social-media");
+
+//Event Listeners
+todoButton.addEventListener("click", moveIcon);
+
+//Functions
+
+function moveIcon(e) {
+  const todoDiv = document.createElement("div");
+}
